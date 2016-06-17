679ffd606f4e9f2e445b6ccca038370e972168be
Kaustubh Vaidya <kaustub.vaidya@gmail.com>
2016-06-17 18:26:19 +0530

    fixed bindable path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let contextPath = process.cwd();
 let _ = require('underscore');
-let Bindable = require(contextPath + '/cmd-adapter/bindable.js');
+let Bindable = require('bindable.js');
 
 class CmdAdapter extends Bindable {
 
